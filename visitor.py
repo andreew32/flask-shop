5b3de47327e8181eb472ed2bf62408244e2b20fe
andreew32 <66839175+andreew32@users.noreply.github.com>
2024-03-31 13:40:03 +0500

    Added tanavigation panel - view, template, sql query, css

--- visitor.py
+++ visitor.py
@@ -12,13 +12,35 @@ from .db import get_db
 
 bp = Blueprint('visitor', __name__, url_prefix='/visitor')
 
+def getcatssets():
+  cur = get_db().cursor(cursor_factory=psycopg2.extras.DictCursor)
+  cur.execute("SELECT * FROM category")
+  cats = cur.fetchall()
+  cur.execute("SELECT * FROM set")
+  sets = cur.fetchall()
+  catssets = {}
+  def getcats(set_id):
+    x = {}
+    for cat in cats:
+      if cat['set_id'] == set_id:
+        x[cat['title']] = cat['id']
+    return x
+  for set in sets:
+    id = set['id']
+    catssets[set['title']] = {'id': id, 'categories': getcats(id)}
+  cur.close()
+  return catssets
+
+def render_template_nav(template, **context):
+  return render_template(template, sets=getcatssets(), **context)
+
 @bp.route('/categories', methods=('GET',))
 def categories():
   cur = get_db().cursor(cursor_factory=psycopg2.extras.DictCursor)
   error = None
   cur.execute("SELECT * FROM category")
   categories = cur.fetchall()
-  return render_template('visitor/categories.html', categories=categories)
+  return render_template_nav('visitor/categories.html', categories=categories)
 
 @bp.route('/category/<int:id>', methods=('GET',))
 def category(id):
@@ -28,25 +50,19 @@ def category(id):
               " ON f.category_id = (%s) AND f.merch_id = m.id", (id,)
   )
   prods = cur.fetchall()
-  return render_template('visitor/category.html', prods=prods, catid=id)
+  return render_template_nav('visitor/category.html', prods=prods, catid=id)
 
 @bp.route('/category/<int:id1>/prod/<int:id2>', methods=('GET',))
 def prod(id1, id2):
   cur = get_db().cursor(cursor_factory=psycopg2.extras.RealDictCursor)
   error = None
-  #cur.execute("SELECT * FROM merch WHERE id = (%s)", (id2,))
-  #cur.execute(
-  #      "SELECT m.title, m.content, p.filename"
-  #      " FROM merch m FULL JOIN pic p"
-  #      " ON m.id = p.merch_id AND m.id = (%s)", (id2,)
-  #)
   cur.execute("SELECT m.title, m.content FROM merch m WHERE m.id = (%s)", (id2,))
   prod = cur.fetchone()
   cur = get_db().cursor(cursor_factory=psycopg2.extras.DictCursor)
   cur.execute("SELECT p.filename FROM pic p WHERE p.merch_id = (%s)", (id2,))
   filenames = cur.fetchall()
   prod['filenames'] = list(map(lambda row: row[0], filenames))
-  return render_template('/visitor/prod.html', prod=prod)
+  return render_template_nav('/visitor/prod.html', prod=prod)
 
 @bp.route('/set/<int:id>', methods=('GET',))
 def set(id):
@@ -58,7 +74,7 @@ def set(id):
               (id,)
   )
   prods = cur.fetchall()
-  return render_template('visitor/set.html', prods=prods, setid=id)
+  return render_template_nav('visitor/set.html', prods=prods, setid=id)
 
 @bp.route('/uploads/<name>', methods=('GET', 'POST'))
 def download_pic(name):
